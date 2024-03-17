65012eec54b12c9bab977e185b92fa1b13d5f15b
wenlong dong <wenldong666@outlook.com>
2024-03-18 00:37:29 +0800

    feat: 合并有序链表

---
+++ leetcode/editor/cn/[21]合并两个有序链表.cpp
@@ -0,0 +1,58 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+//  Definition for singly-linked list.
+struct ListNode {
+    int val;
+    ListNode *next;
+
+    ListNode() : val(0), next(nullptr) {}
+
+    ListNode(int x) : val(x), next(nullptr) {}
+
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode *mergeTwoLists(ListNode *list1, ListNode *list2) {
+        auto *dummy = new ListNode(-1);
+        ListNode *p = dummy;
+        ListNode *p1 = list1;
+        ListNode *p2 = list2;
+        while (p1 != nullptr && p2 != nullptr) {
+            if (p1->val <= p2->val) {
+                p->next = p1;
+                p1 = p1->next;
+            } else {
+                p->next = p2;
+                p2 = p2->next;
+            }
+            p = p->next;
+        }
+
+        if (p1 != nullptr) {
+            p->next = p1;
+        }
+
+        if (p2 != nullptr) {
+            p->next = p2;
+        }
+
+        return dummy->next;
+    }
+};
+//leetcode submit region end(Prohibit modification and deletion)
