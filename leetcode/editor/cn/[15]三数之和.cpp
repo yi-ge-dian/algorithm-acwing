7d869281ece7c82fb109a6706464ecfe2da1f02d
Wenlong Dong <wenldong666@outlook.com>
2024-03-28 11:22:27 +0800

    feat: [15]三数之和

--- leetcode/editor/cn/[15]三数之和.cpp
+++ leetcode/editor/cn/[15]三数之和.cpp
@@ -1,11 +1,42 @@
 #include<iostream>
 #include<vector>
+
 using namespace std;
+
 //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
 public:
-    vector<vector<int>> threeSum(vector<int>& nums) {
-        
+    vector<vector<int>> threeSum(vector<int> &nums) {
+        vector<vector<int>> ans;
+
+        sort(nums.begin(), nums.end());
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] > 0) {
+                return ans;
+            }
+
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+
+            int l = i + 1;
+            int r = nums.size() - 1;
+            while (l < r) {
+                if (nums[i] + nums[l] + nums[r] < 0) {
+                    l++;
+                } else if (nums[i] + nums[l] + nums[r] > 0) {
+                    r--;
+                } else {
+                    ans.push_back({nums[i], nums[l], nums[r]});
+                    while (l < r && nums[l] == nums[l + 1]) l++;
+                    while (l < r && nums[r] == nums[r - 1]) r--;
+
+                    l++;
+                    r--;
+                }
+            }
+        }
+        return ans;
     }
 };
 //leetcode submit region end(Prohibit modification and deletion)
